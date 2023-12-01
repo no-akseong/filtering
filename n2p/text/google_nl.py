56d4ff57e3e1405317baf618b70da572e1bd7326
hohihanhyeon <hohihanhyeon@gmail.com>
2023-12-01 17:44:48 +0900

    기타 수정

--- n2p/text/google_nl.py
+++ n2p/text/google_nl.py
@@ -6,3 +6,9 @@ def sentiment_score(text):
     document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
     sentiment = client.analyze_sentiment(request={"document": document}).document_sentiment
     return sentiment.score
+
+# 메인
+if __name__ == "__main__":
+    import app
+    app.setup()
+    print(sentiment_score("이거 똑바로 작동 안해? 나한테 죽을래!!!"))
